8ad2868e02a94e85be99f95ef7998ec4d92048f9
zeeshan966 <zeeshanbhai@gmail.com>
2025-06-04 00:28:00 +0530

    footer added

--- first.js
+++ first.js
@@ -2,3 +2,7 @@ const button = "Added a buttob";
 console.log(button);
 const login = "Login page added";
 console.log(login);
+
+
+const footer = "footer added in our website";
+console.log(footer);

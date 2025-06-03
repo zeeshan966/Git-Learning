9fff6532b923a51cec180ea4a8a80c8d7f4afc66
zeeshan966 <zeeshanbhai@gmail.com>
2025-06-04 00:23:00 +0530

    payment updated after footer

--- first.js
+++ first.js
@@ -2,7 +2,12 @@ const button = "Added a buttob";
 console.log(button);
 const login = "Login page added";
 console.log(login);
+<<<<<<< HEAD
 
 
 const footer = "footer added in our website";
 console.log(footer);
+=======
+const Payment = "Integrated the payment gateway";
+console.log(Payment);
+>>>>>>> c4e911e (payment added)

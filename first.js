c4a0f6dcdf4bfbbcdf517f2c5df1cad23c438ea0
zeeshan966 <zeeshanbhai@gmail.com>
2025-06-05 00:17:43 +0530

    Latest Changes

--- first.js
+++ first.js
@@ -2,17 +2,10 @@ const button = "Added a buttob";
 console.log(button);
 const login = "Login page added";
 console.log(login);
-<<<<<<< HEAD
 
 
 const footer = "footer added in our website";
 console.log(footer);
-=======
 const Payment = "Integrated the payment gateway";
 console.log(Payment);
-<<<<<<< HEAD
->>>>>>> c4e911e (payment added)
-=======
-const upi = "Integrated the upi also";
-console.log(upi);
->>>>>>> 20af249 (upi added)
+console.log("Latest Update");
